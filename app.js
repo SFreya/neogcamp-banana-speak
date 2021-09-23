bf559fbb441939f25113137ec09196ec7c0172fc
S_Sakshi <freyas8780@gmail.com>
2021-09-23 15:39:46 +0530

    ip/op process done

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var btnTranslate=document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
 console.log(txtInput)
 
 function clickEventHnadler()
 {
-    console.log("clicked");
-    console.log("input", txtInput.value);
+    //console.log("clicked");
+    //console.log("input", txtInput.value);
+
+    outputDiv.innerText = "ananabbanana  " + txtInput.value;
 }
 
 btnTranslate.addEventListener("click",clickEventHnadler)
